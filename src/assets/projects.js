fb93ab0ff830033d321d242bb39802cddf336b76
bradleyhop <bradleyhop@gmail.com>
2021-04-27 15:45:16 -0500

    add image assets; project file

---
+++ src/assets/projects.js
@@ -0,0 +1,233 @@
+const projects = {
+  d3: {
+    title: "Data Visualization with D3",
+    copy: "Lorum impsum",
+    projects: {
+      certificate: {
+        title: "Data Visualization Certification",
+        link: "https://www.freecodecamp.org/certification/bradleyhop/data-visualization",
+        img: {
+          src: "",
+          alt: "",
+        },
+        github: "https://github.com/bradleyhop",
+      },
+      treemap: {
+        title: "Treemap Diagram",
+        link: "https://bradleyhop.github.io/d3-treemap-build/",
+        img: {
+          src: "",
+          alt: "",
+        },
+        github: "https://github.com/bradleyhop/d3-treemap-diagram",
+      },
+      choropleth: {
+        title: "Choropleth Map",
+        link: "https://bradleyhop.github.io/d3-choropleth-build/",
+        img: {
+          src: "",
+          alt: "",
+        },
+        github: "https://github.com/bradleyhop/d3-choropleth",
+      },
+      heatmap: {
+        title: "Heat Map",
+        link: "https://bradleyhop.github.io/d3-heatmap-build/",
+        img: {
+          src: "",
+          alt: "",
+        },
+        github: "https://github.com/bradleyhop/d3-heat-map",
+      },
+      scatterplot: {
+        title: "Scatterplot Graph",
+        link: "https://bradleyhop.github.io/d3-scatterplot-build/",
+        img: {
+          src: "",
+          alt: "",
+        },
+        github: "https://github.com/bradleyhop/d3-scatter-plot",
+      },
+      bar: {
+        title: "Bar Chart",
+        link: "https://bradleyhop.github.io/d3-barchart-build/",
+        img: {
+          src: "",
+          alt: "",
+        },
+        github: "https://github.com/bradleyhop/d3-bar-chart",
+      },
+    },
+  },
+  responsiveWebDesign: {
+    title: "Responsive Web Design",
+    copy: "The first certification of the freeCodeCamp curriculum. This section teaches html and css to complete five projects each with their own use story requirements.  On each project, there is a green hamburger icon in the top left corner that opens the testing suite for that particular project. The user stories are listed therein (be sure that the correct project is selected in the drop-down menu).",
+    projects:
+    {
+      certificate: {
+        title: "Responsive Web Design Certification",
+        link: "https://www.freecodecamp.org/certification/bradleyhop/responsive-web-design",
+        img: {
+          src: "assets/responsiveCert.png",
+          alt: "certificate for responsive web design",
+        },
+        githubLink: ""
+      },
+      porfolio: {
+        title: "Personal Profile",
+        link: "https://bradleyhop.github.io/",
+        img: {
+          src: "assets/portfolio.png",
+          alt: "screenshot of portfolio webpage",
+        },
+        githubLink: "https://github.com/bradleyhop/bradleyhop.github.io",
+      },
+      techDoc:{
+        title: "Technical Documentation" ,
+        link: "https://bradleyhop.github.io/techDoc/techDoc.html",
+        img: {
+          src: "assets/fccTechDoc.png",
+          alt: "screenshot of technical documentation project",
+        },
+        githubLink: "https://github.com/bradleyhop/bradleyhop.github.io/tree/master/techDoc",
+      },
+      landingPage: {
+        title: "Product Landing Page",
+        link: "https://bradleyhop.github.io/productLandingPage/productLandingPage.html",
+        img: {
+          src: "assets/fccProductLanding.png",
+          alt: "screenshot of product landing page",
+        },
+        github: "https://github.com/bradleyhop/bradleyhop.github.io/tree/master/productLandingPage",
+      },
+      survey: {
+        title: "Survey Form",
+        link: "https://bradleyhop.github.io/surveyForm/surveyForm.html",
+        img: {
+          src: "assets/survey.png",
+          alt: "screenshot of survey form",
+        },
+        github: "https://github.com/bradleyhop/bradleyhop.github.io/tree/master/surveyForm",
+      },
+      tribute: {
+        title: "Tribute",
+        link: "https://bradleyhop.github.io/tribute/tribute.html",
+        img: {
+          src: "assets/tribute.png",
+          alt: "screenshot of tribute webpage to Frankie Manning",
+        },
+        github: "https://github.com/bradleyhop/bradleyhop.github.io/tree/master/tribute",
+      },
+    },
+  },
+  javascript: {
+    title: "Javascript Algorithms and Data Structures",
+    copy: "The second certification of the freeCodeCamp curriculum. This sections teaches javascript syntax and data structures. I have solved all of the three levels of algorithm challenges. This section deals with solely with learning basic programming skills through javascript.",
+    projects: {
+      js: {
+        title: "Javascript Algorithms and Data Structures" ,
+        link: "https://github.com/bradleyhop/freeCodeCampAlgorithms",
+        img: {
+          src: "assets/jsCert.png",
+          alt: "javascript algorithm certification",
+        },
+        github: "https://github.com/bradleyhop/freeCodeCampAlgorithms",
+      },
+    }
+  },
+  frontEnd: {
+    title: "Front End Libraries Certification",
+    copy: "The third certification of the freeCodeCamp curriculum. This section teaches the use of various libraries for html, css, and javascript. On each project, there is a green hamburger icon in the top left corner that opens the testing suite for that particular project. The user stories are listed therein (be sure that the correct project is selected in the drop-down menu).",
+    projects: {
+      certificate: {
+        title: "Front End Libraries Certification",
+        link: "https://www.freecodecamp.org/certification/bradleyhop/front-end-libraries",
+        img: {
+          src: "assets/frontEndLibraries.png",
+          alt: "screenshot of front end libraries certificate",
+        },
+        github: "https://github.com/bradleyhop",
+      },
+      pomo: {
+        title: "Pomodoro Timer",
+        link: "https://bradleyhop.github.io/pomodoroTimer/",
+        img: {
+          src: "assets/pomodoroTimer.png",
+          alt: "screenshot of pomodoro timer app",
+        },
+        github: "https://github.com/bradleyhop/bradleyhop.github.io/tree/master/pomodoro-timer/",
+      },
+      calc: {
+        title: "Javascript Calculator",
+        link: "https://bradleyhop.github.io/js-calculator/",
+        img: {
+          src: "assets/js-calculator.png",
+          alt: "screenshot of markdown to javascript calculator",
+        },
+        github: "https://github.com/bradleyhop/bradleyhop.github.io/tree/master/js-calculator/",
+      },
+      drum: {
+        title: "Drum Machine",
+        link: "https://bradleyhop.github.io/drumMachine/",
+        img: {
+          src: "assets/drum-machine.png",
+          alt: "screenshot of game boy inspired drum machine",
+        },
+        github: "https://github.com/bradleyhop/bradleyhop.github.io/tree/master/drum-machine/",
+      },
+      markdown: {
+        title: "Markdown Previewer",
+        link: "https://bradleyhop.github.io/markdownPreviewer/markdownPre.html",
+        img: {
+          src: "assets/markdownPreviewer.png",
+          alt: "screenshot of markdown to html previewer",
+        },
+        github: "https://github.com/bradleyhop/bradleyhop.github.io/tree/master/markdownPreviewer/",
+      },
+      random:{
+        title: "Random Quote Machine",
+        link: "https://bradleyhop.github.io/randomQuotes/randomQuote.html",
+        img: {
+          src: "assets/randomQuoteGenerator.png",
+          alt: "screenshot of random quote machine",
+        },
+        github: "https://github.com/bradleyhop/bradleyhop.github.io/tree/master/randomQuotes/",
+      },
+    },
+  },
+  extra: {
+    title: "Extracirricular",
+    copy: "In addition to freeCodeCamp, I use other resources to increase my knowledge and practice my skills. Here are various algorithm exercises, classes, and projects not part of the freeCodeCamp curriculum.",
+    projects: {
+      postRock: {
+        title: "Post-Rock Band Name Generator" ,
+        link: "https://bradleyhop.github.io/postRockBandName/",
+        img: {
+          src: "assets/postrock-bandname.jpg",
+          alt: "screenshot of post rock band name quiz",
+        },
+        github: "https://github.com/bradleyhop/postrock-bandname/",
+      },
+      termQuote: {
+        title: "Terminal Quote Generator",
+        link: "https://bradleyhop.github.io/randomQuotes/extraCreditRQ/random.html",
+        img: {
+          src: "assets/terminalQuote.png",
+          alt: "screenshot of terminal style random quote generator",
+        },
+        github: "https://github.com/bradleyhop/bradleyhop.github.io/tree/master/randomQuotes/extraCreditRQ/",
+      },
+      wikisearch: {
+        title: "Wikipedia Article Search and Preview" ,
+        link: "https://bradleyhop.github.io/wikiSearchApp/",
+        img: {
+          src: "assets/wikiSearchApp.png",
+          alt: "screenshot of wikipedia search and preview article application",
+        },
+        github: "https://github.com/bradleyhop/wikiSearchApp/",
+      },
+    },
+  }
+};
+
+console.log(projects.responsiveWebDesign.projects.img.src);
