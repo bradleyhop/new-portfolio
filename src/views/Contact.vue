afbc5bf0c7ba7c92215f4c3366c67ce45d310cf4
bradleyhop <bradleyhop@gmail.com>
2021-05-02 13:32:51 -0500

    contact page view

---
+++ src/views/Contact.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'Contact',
+};
+</script>
+
+<template>
+  <v-container
+    class="fill-height"
+    fluid>
+    <v-row>
+      <v-col>
+        <p>This is the contact page</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>>
