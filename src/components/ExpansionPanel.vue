4556a43a9a12ca41a18fc505ea273ad78c3be211
bradleyhop <bradleyhop@gmail.com>
2021-05-02 13:32:17 -0500

    portfolio nested expansion panel setup; contact view

---
+++ src/components/ExpansionPanel.vue
@@ -0,0 +1,57 @@
+<script>
+import projects from '@/assets/projects.js';
+
+export default {
+  name: 'ExpansionPanel',
+  data() {
+    return {
+      projectHeading: projects,
+    }
+  },
+};
+</script>
+
+<template>
+  <v-row>
+    <v-col
+      lg="8" sm="12"
+      >
+  <v-expansion-panels>
+    <v-expansion-panel
+      v-for="item in projectHeading"
+      :key="item.i"
+    >
+      <v-expansion-panel-header>
+        {{ item.title }}
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        {{ item.copy }}
+        <!-- nested expansion panels for projects -->
+        <v-expansion-panels>
+          <v-expansion-panel
+              v-for="projectItem in item.projects"
+              :key="projectItem.i"
+              >
+              <v-expansion-panel-header>
+                {{ projectItem.title }}
+              </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <a :href="projectItem.link"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                gitHub
+              </a>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+        <!-- end nested panels -->
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+  </v-col>
+  </v-row>
+</template>
+
+<style lang="scss">
+</style>>
