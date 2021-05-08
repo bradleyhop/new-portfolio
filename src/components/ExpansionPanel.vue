c5cd9337c32298d37a878d75f5e68b15dfc50933
Bradley Smith <bradleyhop@gmail.com>
2021-05-08 18:50:37 -0500

    add images; update project links; colors

--- src/components/ExpansionPanel.vue
+++ src/components/ExpansionPanel.vue
@@ -6,7 +6,7 @@ export default {
 
   data:() => ({
       projectListings: ouvre, // project data to iterate
-      overlay: false, // state of hover for image overlay for links
+      overlay: false, // state of hover for image overlay for project links
     }),
 
 };
@@ -30,68 +30,65 @@ export default {
           </div>
           <!-- nested expansion panels for projects -->
           <v-expansion-panels>
-              <v-expansion-panel
-                  v-for="(projectItem, j) in item.projects"
-                  :key="j"
-                  >
-                  <v-expansion-panel-header>
-                    {{ projectItem.title }}
-                    <template v-slot:actions>
-                      <v-icon color="secondary">$expand</v-icon>
-                    </template>
-                  </v-expansion-panel-header>
-                  <v-expansion-panel-content>
-                    <!-- NB: all images must be present as requested otherwise the
-                      component will not render! -->
+            <v-expansion-panel
+                v-for="(projectItem, j) in item.projects"
+                :key="j"
+                >
+                <v-expansion-panel-header>
+                  <i>
+              {{ projectItem.title }}
+                  </i>
+              <template v-slot:actions>
+                <v-icon color="secondary">$expand</v-icon>
+              </template>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-hover>
+                    <template v-slot:default="{ hover }">
 
-                      <div>
-                      <v-hover>
-                        <template v-slot:default="{ hover }">
+                      <div><!-- div needed for v-hover to work -->
+                        <v-img v-if="projectItem.img.src"
+                               :src="require(`@/assets/screenshots/${projectItem.img.src}?vuetify-preload`)"
+                               :alt="projectItem.img.alt"
+                               :aspect-ratio="16/9"
+                               class="mx-auto"
+                               width="30em"
+                               >
+                        </v-img>
 
-                          <div>
-                          <v-img v-if="projectItem.img.src"
-                                 :src="require(`@/assets/screenshots/${projectItem.img.src}?vuetify-preload`)"
-                                 :alt="projectItem.img.alt"
-                                 :aspect-ratio="16/9"
-                                 class="mx-auto"
-                                 width="30em"
-                                 >
-                          </v-img>
-
-                            <!-- hover overlay -->
-                            <v-fade-transition>
-                              <v-overlay
-                                  v-if="hover"
-                                  absolute
-                                  color="grey darken-1"
-                                  >
-                                  <a :href="projectItem.link"
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                  >
-                                  <v-icon size="8rem" class="icon-link">
-                                    mdi-link
-                                  </v-icon>
-                                  </a>
-
-                                <a v-if="projectItem.github"
-                                   :href="projectItem.github"
-                                   target="_blank"
-                                   rel="noopener noreferrer"
-                                   >
-                                   <v-icon size="6rem" class="icon-link">
-                                     mdi-github </v-icon>
+                          <!-- hover overlay -->
+                          <v-fade-transition>
+                            <v-overlay
+                                v-if="hover"
+                                absolute
+                                color="grey darken-1"
+                                >
+                                <a :href="projectItem.link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
+                                <v-icon size="8rem" class="icon-link">
+                                  mdi-link
+                                </v-icon>
                                 </a>
-                              </v-overlay>
-                            </v-fade-transition>
 
-                            </div>
-                        </template>
-                      </v-hover>
-                      </div>
+                              <a v-if="projectItem.github"
+                                 :href="projectItem.github"
+                                 target="_blank"
+                                 rel="noopener noreferrer"
+                                 >
+                                 <v-icon size="6rem" class="icon-link">
+                                   mdi-github
+                                 </v-icon>
+                              </a>
+                            </v-overlay>
+                          </v-fade-transition>
+                      </div><!-- div for v-hover -->
+                    </template>
+                  </v-hover>
 
-                  </v-expansion-panel-content>
-              </v-expansion-panel>
+                </v-expansion-panel-content>
+            </v-expansion-panel>
           </v-expansion-panels>
           <!-- end nested panels -->
         </v-expansion-panel-content>
