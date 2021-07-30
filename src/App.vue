56ee0efd22e1bdeb5e599e934ec2166fb5fbcc41
Bradley Smith <bradleywjsmith@gmail.com>
2021-07-30 16:49:10 -0500

    edit twitter img; add footer img on navbar

--- src/App.vue
+++ src/App.vue
@@ -14,59 +14,69 @@ export default {
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app right temporary color="primaryLight">
-      <v-list class="view-list">
-        <router-link :to="{ name: 'Home' }">
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon color="secondary">mdi-home</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>
-                Home
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link>
+      <div class="nav-container">
+        <div class="nav-list-container">
+          <v-list class="view-list">
+            <router-link :to="{ name: 'Home' }">
+              <v-list-item link>
+                <v-list-item-action>
+                  <v-icon color="secondary">mdi-home</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Home
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </router-link>
 
-        <router-link :to="{ name: 'About' }">
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon color="secondary">mdi-cards-heart</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>
-                About Me
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link>
+            <router-link :to="{ name: 'About' }">
+              <v-list-item link>
+                <v-list-item-action>
+                  <v-icon color="secondary">mdi-cards-heart</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    About Me
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </router-link>
 
-        <router-link :to="{ name: 'Portfolio' }">
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon color="secondary">mdi-code-tags</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>
-                Portfolio
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link>
+            <router-link :to="{ name: 'Portfolio' }">
+              <v-list-item link>
+                <v-list-item-action>
+                  <v-icon color="secondary">mdi-code-tags</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Portfolio
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </router-link>
 
-        <router-link :to="{ name: 'Contact' }">
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon color="secondary">mdi-email</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>
-                Contact
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link>
-      </v-list>
+            <router-link :to="{ name: 'Contact' }">
+              <v-list-item link>
+                <v-list-item-action>
+                  <v-icon color="secondary">mdi-email</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Contact
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </router-link>
+          </v-list>
+        </div>
+        <!-- nav-list-container -->
+
+        <div class="nav-img">
+          <v-img src="@/assets/og.jpg" contain> </v-img>
+        </div>
+      </div>
+      <!-- nav-container -->
     </v-navigation-drawer>
 
     <v-app-bar app dense elevate-on-scroll color="primary">
@@ -92,7 +102,21 @@ export default {
 </template>
 
 <style lang="scss">
+.nav-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.nav-list-container {
+  flex: 1 0 auto;
+}
+
 .view-list {
   margin-top: 2rem;
+}
+
+.nav-img {
+  flex-shrink: 0;
 }
 </style>
